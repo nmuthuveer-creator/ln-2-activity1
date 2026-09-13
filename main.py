41f0ae2713319ef3754aa3721638f414b3630a37
nmuthuveer-creator <nmuthuveer@gmail.com>
2026-09-13 19:27:31 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#print a sentence
+print("hello welcome to the world of program")
+
+#print a number
+print(4)
+
+#next line after print
+print("hello my name n muthuveer \n")
+
+#pass multiple arguments for printing
+print("sum of 4 and 3 is", 7)
+
+#end argument of print satement
+print ("welcome to", end="*")
+
+
